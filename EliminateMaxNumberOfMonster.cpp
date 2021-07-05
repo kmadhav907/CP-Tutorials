c6a65fbed287c1863c35a420e827baa4210e7028
Madhav Bhat K <54632221+kmadhav907@users.noreply.github.com>
2021-07-05 13:42:27 +0530

    Create EliminateMaxNumberOfMonster.cpp

---
+++ EliminateMaxNumberOfMonster.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        int n = dist.size();
+        vector<int> time(n);
+        for(int i = 0 ; i < n ; i++){
+            time[i] = dist[i]/speed[i];
+            if(dist[i]%speed[i] > 0){
+                time[i]++;
+            }
+        
+          
+        }   
+        sort(time.begin(), time.end());
+        for(int i = 0 ; i < n ; i++){
+            if(time[i]<=i){
+                return i;
+            }
+        }
+        return n;
+    }
+};
